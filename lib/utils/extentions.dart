82287a98df451e302bfa8fc222de7a8ba6ff14a5
amir1430 <amirhossein13791430@gmail.com>
2023-02-10 12:42:09 +0330

    refactor: delete [hideSnackbar], [showSnackbar], [showGDialog], [showModal] from [ScaffolHelper] and meke them extension on [BuildContext]

--- lib/utils/extentions.dart
+++ lib/utils/extentions.dart
@@ -11,3 +11,77 @@ extension TransactionTypeX on TransactionType {
     }
   }
 }
+
+extension BuildContextX on BuildContext {
+  void hideSnackbar() {
+    ScaffoldMessenger.of(this).hideCurrentSnackBar();
+  }
+
+  void showSnackbar({
+    required Widget content,
+    Color color = Colors.grey,
+  }) {
+    hideSnackbar();
+    ScaffoldMessenger.of(this).showSnackBar(
+      SnackBar(
+        content: content,
+        backgroundColor: color,
+      ),
+    );
+  }
+
+  Future<void> showGDialog({
+    Widget? child,
+    bool useRootNavigator = false,
+    bool barrierDismissible = true,
+    String? barrierLabel,
+    Color barrierColor = Colors.black26,
+    Offset begin = Offset.zero,
+  }) async {
+    await showGeneralDialog(
+      context: this,
+      barrierLabel: barrierLabel ?? 'barrier_label',
+      barrierDismissible: barrierDismissible,
+      barrierColor: barrierColor,
+      pageBuilder: (context, animation, secondaryAnimation) {
+        const end = Offset.zero;
+        const curve = Curves.ease;
+
+        final tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: Dialog(
+            insetPadding:
+                MediaQuery.of(context).viewInsets.copyWith(right: 40, left: 40),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                right: 16,
+                left: 16,
+                top: 12,
+                bottom: 12,
+              ),
+              child: child,
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> showModal({
+    Widget? child,
+    bool useRootNavigator = false,
+  }) async {
+    await showModalBottomSheet<void>(
+      useRootNavigator: useRootNavigator,
+      context: this,
+      builder: (context) {
+        return Padding(
+          padding: MediaQuery.of(context).viewInsets,
+          child: child,
+        );
+      },
+    );
+  }
+}
